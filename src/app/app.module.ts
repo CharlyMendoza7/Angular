f1cb1eaf756e0de530a924359cb1785e65709740
Carlos David Mendoza <carlosmendoza1719@gmail.com>
2021-11-25 18:03:46 -0600

    basic services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,20 @@ import {MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +33,7 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DishService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,10 +1,14 @@
+import { Comment } from './comment';
+
 export class Dish {
     id: string = '';
     name: string = '';
     image: string = '';
     category: string = '';
-    featured: boolean = false;
+    featured?: boolean;
     label: string = '';
     price: string = '';
     description: string = '';
+
+    comments?: Comment[];
 }
